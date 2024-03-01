e3e49d8db5c19782a6376a31fba41d1e035cc3e9
Marlon Griebeler da Hora <marlongriebeler2@gmail.com>
2024-03-01 16:30:28 -0300

    exercise 5.4 on API Authentication

---
+++ Backend/5.4+API+Authentication/index.js
@@ -0,0 +1,73 @@
+import axios from "axios";
+import express from "express";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com/";
+
+//TODO 1: Fill in your values for the 3 types of auth.
+const yourUsername = "marlov";
+const yourPassword = "marlov";
+const yourAPIKey = "ea4fc72d-a221-43f9-aa0a-1c6c377f0d5d";
+const yourBearerToken = "ab67304b-a6e6-4b81-a1aa-fe68ab69b1ec";
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { content: "API Response." });
+});
+
+app.get("/noAuth", async (req, res) => {
+  try {
+    const response = await axios.get(API_URL + "random");
+    res.render("index.ejs", { content: JSON.stringify(response.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: error.response.data });
+  }
+});
+
+app.get("/basicAuth", async (req, res) => {
+  try {
+    const response = await axios.get(API_URL + "all", {
+      auth: {
+        username: yourUsername,
+        password: yourPassword,
+      },
+    });
+    res.render("index.ejs", { content: JSON.stringify(response.data) });
+  } catch (error) {
+    console.log(error);
+    res.render("index.ejs", { content: error.response.data });
+  }
+});
+
+app.get("/apiKey", async (req, res) => {
+  try {
+    const response = await axios.get(API_URL + "filter", {
+      params: {
+        score: 5,
+        apiKey: yourAPIKey,
+      },
+    });
+    res.render("index.ejs", { content: JSON.stringify(response.data) });
+  } catch (error) {
+    console.log(error);
+    res.render("index.ejs", { content: error.response.data });
+  }
+});
+
+app.get("/bearerToken", async (req, res) => {
+  try {
+    const response = await axios.get(API_URL + "secrets/42", {
+      headers: {
+        Authorization: `Bearer ${yourBearerToken}`,
+      },
+    });
+    res.render("index.ejs", { content: JSON.stringify(response.data) });
+  } catch (error) {
+    console.log(error);
+    res.render("index.ejs", { content: error });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
