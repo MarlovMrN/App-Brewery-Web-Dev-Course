d1eee63adaecb236afcd579076accc5ce9c33ad7
Marlon Griebeler da Hora <marlongriebeler2@gmail.com>
2024-03-21 00:34:12 -0300

    using map to loop through the contacts

--- React/296-react-props-practice/src/components/App.jsx
+++ React/296-react-props-practice/src/components/App.jsx
@@ -20,24 +20,16 @@ function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        number={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        number={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        number={contacts[2].phone}
-        email={contacts[2].email}
-      />
+
+      {contacts.map((contact, index) => (
+        <Card
+          key={index}
+          name={contact.name}
+          img={contact.imgURL}
+          number={contact.phone}
+          email={contact.email}
+        />
+      ))}
     </div>
   );
 }
