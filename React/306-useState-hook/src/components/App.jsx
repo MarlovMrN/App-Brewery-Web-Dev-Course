b669ab5a5cd9e42e02cd3e14e17aa61c23eee262
Marlon Griebeler da Hora <marlongriebeler2@gmail.com>
2024-03-22 18:55:45 -0300

    306 on useState hook complete

--- React/306-useState-hook/src/components/App.jsx
+++ React/306-useState-hook/src/components/App.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+    </div>
+  );
 }
 
 export default App;
