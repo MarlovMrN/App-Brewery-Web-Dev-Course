21e3b515c557ce4356e7b9b9cbbd47616a3a796d
Marlon Griebeler da Hora <marlongriebeler2@gmail.com>
2024-03-22 16:46:03 -0300

    300 on map, reduce, filter , find and findIndex

--- React/300-map-filter-reduce/src/index.jsx
+++ React/300-map-filter-reduce/src/index.jsx
@@ -1,21 +1,34 @@
 import emojipedia from "./emojipedia";
 
+//Map -Create a new array by doing something with each item in an array.
 const onlyMeaning = emojipedia.map((entry) => entry.meaning.substring(0, 100));
 
 console.log(onlyMeaning);
 
-//Map -Create a new array by doing something with each item in an array.
-
 //Filter - Create a new array by keeping the items that return true.
+
+const filtered = emojipedia.filter((entry) => entry.meaning.includes("pray"));
+
+console.log(filtered);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
 
+const initialValue = "";
+const reduced = emojipedia.reduce(
+  (accumulator, entry) => accumulator.concat(entry.emoji),
+  initialValue
+);
+
+console.log(reduced);
+
 //Find - find the first item that matches from an array.
+
+const find = emojipedia.find((entry) => entry.emoji === "💪");
+
+console.log(find);
 
 //FindIndex - find the index of the first item that matches.
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+const index = emojipedia.findIndex((entry) => entry.emoji === "💪");
+
+console.log(index);
