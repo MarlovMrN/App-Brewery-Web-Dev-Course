9b646627042ca5a9866d4e0f39dd7a52a5b765f0
Marlon Griebeler da Hora <marlongriebeler2@gmail.com>
2024-03-23 19:17:11 -0300

    311 on react forms

--- React/311-react-forms/src/components/App.jsx
+++ React/311-react-forms/src/components/App.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+
+  function handleSubmit(event) {
+    /// do whatever is needed
+    console.log("form submitted");
+    event.preventDefault();
+  }
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {name} </h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
