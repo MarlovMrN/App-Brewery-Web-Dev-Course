5c64c1bcddcdb8535a1aa044b708914c25801bd5
Marlon Griebeler da Hora <marlongriebeler2@gmail.com>
2024-03-23 20:03:05 -0300

    313 complete on compelx state manage

--- React/313-changing-complex-state/src/components/App.jsx
+++ React/313-changing-complex-state/src/components/App.jsx
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setFullName((prevValue) => {
+      if (name === "fName") {
+        return { fName: value, lName: prevValue.lName };
+      } else if (name === "lName") {
+        return { fName: prevValue.fName, lName: value };
+      }
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          name="fName"
+          placeholder="First Name"
+          value={fullName.fName}
+          type="text"
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          placeholder="Last Name"
+          value={fullName.lName}
+          type="text"
+        />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
