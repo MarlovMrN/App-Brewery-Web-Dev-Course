f99567c425638a2645c551f05148597897977d8e
Marlon Griebeler da Hora <marlongriebeler2@gmail.com>
2024-03-23 20:11:46 -0300

    314 on complex state management done

--- React/314-changing-complex-state-practice/src/components/App.jsx
+++ React/314-changing-complex-state-practice/src/components/App.jsx
@@ -4,8 +4,39 @@ function App() {
   const [contact, setContact] = useState({
     fName: "",
     lName: "",
-    email: ""
+    email: "",
   });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setContact((prevValue) => {
+      switch (name) {
+        case "fName":
+          return {
+            fName: value,
+            lName: prevValue.lName,
+            email: prevValue.email,
+          };
+        case "lName":
+          return {
+            fName: prevValue.fName,
+            lName: value,
+            email: prevValue.email,
+          };
+        case "email":
+          return {
+            fName: prevValue.fName,
+            lName: prevValue.lName,
+            email: value,
+          };
+      }
+    });
+  }
 
   return (
     <div className="container">
@@ -13,11 +44,26 @@ function App() {
         Hello {contact.fName} {contact.lName}
       </h1>
       <p>{contact.email}</p>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
-        <button>Submit</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          value={contact.fName}
+          name="fName"
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          value={contact.lName}
+          name="lName"
+          placeholder="Last Name"
+        />
+        <input
+          onChange={handleChange}
+          value={contact.email}
+          name="email"
+          placeholder="Email"
+        />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
